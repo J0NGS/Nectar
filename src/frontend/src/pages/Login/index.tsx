df541e9d041f6a607e9ddcb66db22773db010666
valdir.junior90 <slipkinoot@hotmail.com>
2024-12-02 19:52:03 -0300

    feat. loginpage

--- src/frontend/src/pages/Login/index.tsx
+++ src/frontend/src/pages/Login/index.tsx
@@ -1,43 +1,11 @@
-import { Button, Input } from "antd";
+import Login from "../../components/organisms/Login";
 
-const Login = () => {
+const LoginPage = () => {
   return (
-    <div className="h-screen flex">
-      <div className="flex flex-col justify-center items-center my-auto mx-auto border p-6">
-        <h1 className="font-bold text-3xl">Bem vindo(a) de volta!</h1>
-        <div className="flex flex-col gap-3 mt-3 w-full">
-          <div className="flex flex-col">
-            <label htmlFor="email">Email</label>
-            <Input
-              className="h-10"
-              id="email"
-              type="email"
-              placeholder="Digite seu e-mail"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="password">Senha</label>
-            <Input
-              className="h-10"
-              id="password"
-              type="password"
-              placeholder="Digite sua senha"
-            />
-          </div>
-          <div className="flex items-center gap-1">
-            <Input
-              type="checkbox"
-              value="false"
-              id="remember"
-              className="h-4 w-4"
-            />
-            <label htmlFor="remember">Lembrar de mim</label>
-          </div>
-          <Button type="primary">Entrar</Button>
-        </div>
-      </div>
-    </div>
+    <>
+      <Login />
+    </>
   );
 };
 
-export default Login;
+export default LoginPage;
