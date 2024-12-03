3b845041db8706551320d2d02f533c99eaa0062e
valdir.junior90 <slipkinoot@hotmail.com>
2024-12-03 00:39:00 -0300

    feat. logo

--- src/frontend/src/components/pages/Login/index.tsx
+++ src/frontend/src/components/pages/Login/index.tsx
@@ -14,7 +14,8 @@ export const LoginPage = () => {
             width={300}
           />
         </div>
-        <div className="flex flex-col justify-center my-auto mx-auto p-6 xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-2/3  ">
+        <div className="flex flex-col justify-center items-center my-auto mx-auto p-6 xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-2/3  ">
+          <Image preview={false} src="/logo.png" width={250}/>
           <h1 className="font-bold text-3xl">Bem vindo(a) de volta!</h1>
           <LoginForm />
         </div>
