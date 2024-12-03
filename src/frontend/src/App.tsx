980e4e262cf1d63aa5b5e2ab1f85934fb333a389
valdir.junior90 <slipkinoot@hotmail.com>
2024-12-02 23:11:44 -0300

    feat. react toast added

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ConfigProvider } from "antd";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import theme from "./theme/themeConfig";
 import routes from "./routes";
@@ -8,6 +10,18 @@ const App: React.FC = () => {
   return (
     <Router>
       <ConfigProvider direction="ltr" theme={theme}>
+      <ToastContainer 
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
         <Routes>
         {routes.map(({ path, component: Component }, index) => (
             <Route key={index} path={path} element={<Component />} />
