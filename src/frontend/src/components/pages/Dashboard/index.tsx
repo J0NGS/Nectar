b40c07be4db7d481deb7ca0a1200559f2745a0d1
João Neto <jnetogoncalo@gmail.com>
2025-01-27 20:43:42 -0300

    att painel de estatisticas

--- src/frontend/src/components/pages/Dashboard/index.tsx
+++ src/frontend/src/components/pages/Dashboard/index.tsx
@@ -8,10 +8,9 @@ import {
 import { DashboarService } from "@/services/dashboarService/service";
 import { Pageable } from "@/types";
 import { Job } from "@/types/entitysType";
-import { Card, Col, Flex, Radio, Row } from "antd";
+import { Card, Col, Flex, Radio, Row, Statistic } from "antd";
 import dayjs from "dayjs";
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const FlowGraph = React.lazy(() => import("@/components/molecules/FlowGraph"));
 
@@ -25,6 +24,13 @@ export const DashboardPage: React.FC = () => {
   const [date, setDate] = useState<Date>(new Date());
   const [jobStatus, setJobStatus] = useState<string>("ALL");
 
+  const [monthlyData, setMonthlyData] = useState({
+    collectedWax: 0,
+    completedJobs: 0,
+    inProcessJobs: 0,
+    registeredProducers: 0,
+  });
+
   const fetchJobs = async () => {
     setLoading(true);
     try {
@@ -32,7 +38,6 @@ export const DashboardPage: React.FC = () => {
         status: jobStatus,
         month: date,
       });
-      console.log("fetchJobs [DashboardPage]", data);
       setResource(data);
     } catch (error) {
       console.error("fetchJobs [DashboardPage]", error);
@@ -45,35 +50,24 @@ export const DashboardPage: React.FC = () => {
     setLoading(true);
     try {
       const { data } = await DashboarService.monthlyGraph(date);
-      console.log("fetchGraphData", data);
-
-      const startedServices = data.data?.map((item: ItensGraph) => {
-        return {
-          day: dayjs(item.date).format("DD"),
-          value: item.startedServices,
-          date: item.date,
-          type: "Iniciados",
-        };
-      });
-
-      const wasteOfServices = data.data?.map((item: ItensGraph) => {
-        return {
-          day: dayjs(item.date).format("DD"),
-          value: item.mediaWasteOfServices,
-          date: item.date,
-          type: "Desperdiçado",
-        };
-      });
-
-      const revenueOfServices = data.data?.map((item: ItensGraph) => {
-        return {
-          day: dayjs(item.date).format("DD"),
-          value: item.mediaRevenueOfServices,
-          date: item.date,
-          type: "Desperdiçado",
-        };
-      });
-
+      const startedServices = data.data?.map((item: ItensGraph) => ({
+        day: dayjs(item.date).format("DD"),
+        value: item.startedServices,
+        date: item.date,
+        type: "Iniciados",
+      }));
+      const wasteOfServices = data.data?.map((item: ItensGraph) => ({
+        day: dayjs(item.date).format("DD"),
+        value: item.mediaWasteOfServices,
+        date: item.date,
+        type: "Desperdiçado",
+      }));
+      const revenueOfServices = data.data?.map((item: ItensGraph) => ({
+        day: dayjs(item.date).format("DD"),
+        value: item.mediaRevenueOfServices,
+        date: item.date,
+        type: "Desperdiçado",
+      }));
       setResourceGraph([
         ...startedServices,
         ...wasteOfServices,
@@ -91,16 +85,32 @@ export const DashboardPage: React.FC = () => {
     try {
       const { data } = await DashboarService.monthlyBoard(date);
       setResourceBoard(data);
-
-      console.log("fetchBoardData", data);
     } catch (error) {
-      console.error("fetchGraphData", error);
+      console.error("fetchBoardData", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchMonthlyData = async () => {
+    setLoading(true);
+    try {
+      const data = await DashboarService.getMonthlyData(date); // TO-DO Criar request no back
+      setMonthlyData({
+        collectedWax: data.collectedWax,
+        completedJobs: data.completedJobs,
+        inProcessJobs: data.inProcessJobs,
+        registeredProducers: data.registeredProducers,
+      });
+    } catch (error) {
+      console.error("Erro ao buscar dados mensais", error);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
+    fetchMonthlyData();
     fetchGraphData();
     fetchBoardData();
   }, [date]);
@@ -111,15 +121,42 @@ export const DashboardPage: React.FC = () => {
 
   return (
     <Flex gap={20} vertical>
+      {/* Painel de estatísticas mensais */}
       <Row gutter={[16, 16]}>
         <Col span={24} md={{ span: 6 }}>
-          <Card></Card>
+          <Card>
+            <Statistic
+              title="Cera arrecadada no mês"
+              value={`${monthlyData.collectedWax}Kg`}
+            />
+          </Card>
         </Col>
-        <Col span={24} md={{ span: 18 }}></Col>
+        <Col span={24} md={{ span: 6 }}>
+          <Card>
+            <Statistic title="Concluídos" value={monthlyData.completedJobs} />
+          </Card>
+        </Col>
+        <Col span={24} md={{ span: 6 }}>
+          <Card>
+            <Statistic title="Em processamento" value={monthlyData.inProcessJobs} />
+          </Card>
+        </Col>
+        <Col span={24} md={{ span: 6 }}>
+          <Card>
+            <Statistic
+              title="Produtores cadastrados"
+              value={monthlyData.registeredProducers}
+            />
+          </Card>
+        </Col>
       </Row>
+
+      {/* Gráfico de fluxo de serviço */}
       <Card title="Fluxo de serviço">
         <FlowGraph data={resourceGraph} />
       </Card>
+
+      {/* Histórico de serviços */}
       <Card title="Histórico">
         <Flex gap={20} vertical>
           <Flex gap={8} justify="end">
