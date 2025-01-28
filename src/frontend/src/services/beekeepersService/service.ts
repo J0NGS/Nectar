af606f8fa2d6cc544a538658d05e49495467384e
Edson Alencar <edson.cesar.alencar@gmail.com>
2025-01-27 22:48:32 -0300

    fix. update beekeeper status

--- src/frontend/src/services/beekeepersService/service.ts
+++ src/frontend/src/services/beekeepersService/service.ts
@@ -1,11 +1,16 @@
-import { ResponseDTO } from "@/types";
 import { GenericService } from "../genericService/genericService";
 
 class beekeepersService extends GenericService {
-    disableBeekeeper = async <T>(beekeeperId:string, data:"ACTIVE" | "INACTIVE") => {
-        const res = await this.getApi().put<ResponseDTO<T>,String>(`${this.getURL()}/${beekeeperId}/status`, data);
-        return res;
-    }
+  disableBeekeeper = async (
+    beekeeperId: string,
+    data: "ACTIVE" | "INACTIVE"
+  ) => {
+    const res = await this.getApi().put(
+      `${this.getURL()}/${beekeeperId}/status`,
+      { status: data }
+    );
+    return res;
+  };
 }
 
 export const BeekeepersService = new beekeepersService("/beekeepers");
